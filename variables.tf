097e5a466869871b615d5b5ca9337f0eab3b009f
akhil-hub <54460451+akhil-hub@users.noreply.github.com>
2025-06-28 20:19:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,40 +1,10 @@
-variable "project_id" {
-  description = "newterraformvariables"
-  type        = string
-}
-
-variable "region" {
-  description = "The GCP region"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "zone" {
-  description = "The GCP zone"
-  type        = string
-  default     = "us-central1-a"
-}
-
-variable "instance_name" {
-  description = "Name of the Compute Engine instance"
-  type        = string
-  default     = "demo-instance"
-}
-
-variable "machine_type" {
-  description = "Type of the Compute Engine instance"
-  type        = string
-  default     = "e2-micro"
-}
-
-variable "image" {
-  description = "Boot disk image for the instance"
-  type        = string
-  default     = "debian-cloud/debian-12"
-}
-
-variable "network" {
-  description = "VPC network name"
-  type        = string
-  default     = "default"
-}
+variable "project" {}
+variable "region" {}
+variable "zone" {}
+variable "credentials_file" {}
+variable "new" {}
+variable "machine_type" {}
+variable "boot_disk_image"       {}
+variable "boot_disk_size_gb"     {}
+variable "boot_disk_type"        {}
+variable "boot_disk_auto_delete" {}
