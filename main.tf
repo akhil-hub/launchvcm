8071b9f72b982e4b0a5575b0914564cd48d61f7d
akhil-hub <54460451+akhil-hub@users.noreply.github.com>
2025-06-28 20:17:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.8"
+    }
+  }
+}
+
 provider "google" {
-  project     = var.project_id
+  project     = var.project
   region      = var.region
+  zone        = var.zone
+  credentials = file(var.credentials_file)
+}
+
+resource "google_compute_network" "default" {
+  name = "default-network"
 }
 
 resource "google_compute_instance" "default" {
-  name         = var.instance_name
+  name         = var.new
   machine_type = var.machine_type
   zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = var.image
+      image = var.boot_disk_image
+      size  = var.boot_disk_size_gb
+      type  = var.boot_disk_type
     }
+    auto_delete = var.boot_disk_auto_delete
   }
 
   network_interface {
-    network = var.network
-    access_config {
-    }
+    network = google_compute_network.default.id
+    access_config {}  # Needed for external IP (optional)
   }
 }
+
